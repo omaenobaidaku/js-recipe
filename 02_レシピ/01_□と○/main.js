73dd45729fae00b91602a595bd129da300d60f8d
omaenobaidaku <munechika.yahiro@gmail.com>
2024-08-18 03:14:02 +0900

    CSSで三角形を描いた

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,16 @@
 const figure = document.getElementById("figure")
 
 window.onresize = function () {
-  figure.classList.toggle("rounded")
+  figure.classList.toggle("triangle")
+}
+
+figure.onclick = function () {
+  if (figure.classList.contains("rounded") === true) {
+    figure.classList.remove("rounded")
+    figure.classList.add("triangle")
+  } else if (figure.classList.contains("triangle") === true) {
+    figure.classList.remove("triangle")
+  } else {
+    figure.classList.add("rounded")
+  }
 }
